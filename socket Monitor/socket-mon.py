da87bd7fc9a77389a7c4e7a335b51a0d0df3d4d4
Sunil Kumar Garg <sunilkumar.garg@sjsu.edu>
2017-02-14 22:39:15 -0800

    CSV format

--- socket Monitor/socket-mon.py
+++ socket Monitor/socket-mon.py
@@ -3,15 +3,12 @@ import re
 
 outPuts = psutil.net_connections("tcp")
 
-#print outPuts
-
-
 outPutList = []
 pidList = {}
 for outPut in outPuts:
-	phonePattern = re.compile(r'[^^]*\'(\d+.\d+.\d+.\d+)\D*(\d+)[^^]*\'(\d+.\d+.\d+.\d+)\D*(\d+)\D*\'(\D+)\'\D*(\d+)')
-	if(phonePattern.search(str(outPut)) != None):
-		pidDetail = phonePattern.search(str(outPut)).groups();
+	outputPattern = re.compile(r'[^^]*\'(\d+.\d+.\d+.\d+)\D*(\d+)[^^]*\'(\d+.\d+.\d+.\d+)\D*(\d+)\D*\'(\D+)\'\D*(\d+)')
+	if(outputPattern.search(str(outPut)) != None):
+		pidDetail = outputPattern.search(str(outPut)).groups();
 		outPutList.append(pidDetail)
 		(l, lp, r, rp, c, pid) = pidDetail
 		if pid in pidList.keys():
@@ -23,7 +20,7 @@ for outPut in outPuts:
 outPutList = sorted(outPutList, key=lambda t: int(t[5]))
 outPutList = sorted(outPutList, key=lambda t: pidList[t[5]], reverse=True)
 
-print "%5s\t%30s\t%35s\t%20s" %("pid", "laddr", "raddr", "status")
+print "%s,%s,%s,%s" %("pid", "laddr", "raddr", "status")
 for output in outPutList:
 	(l, lp, r, rp, c, pid) = output
-	print "%5s\t%30s:%s\t%30s:%s\t%20s" % (pid,l,lp,r,rp,c)
+	print "%s,%s:%s,%s:%s,%s" % (pid,l,lp,r,rp,c)
